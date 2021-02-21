e424704abb7492841b5ee881b78285f4b99e1e44
Laguna1 <lagunazoomarket@gmail.com>
2021-02-21 14:08:36 +0200

    Add data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+r1 = Role.create({ name: 'Regular', description: 'Can create rekords' })
+r2 = Role.create({ name: 'Seller', description: 'Can read and update rekords' })
+r3 = Role.create({ name: 'Admin', description: 'Can perform any CRUD operation on any resource' })
+
+u1 = User.create({ name: 'Sally-user', email: 'sally@example.com', password: '11111111', password_confirmation: '11111111', role_id: r1.id })
+u2 = User.create({ name: 'Sue-doctor', email: 'sue@example.com', password: '11111111', password_confirmation: '11111111', role_id: r2.id })
+u3 = User.create({ name: 'Kev-doctor', email: 'kev@example.com', password: '11111111', password_confirmation: '11111111', role_id: r2.id })
+u4 = User.create({ name: 'Jack-admin', email: 'jack@example.com', password: '11111111', password_confirmation: '11111111', role_id: r3.id })
